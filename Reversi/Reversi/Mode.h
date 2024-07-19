3ff8276d075c1047e2ace5f39dc1b39b1420a3f2
kmm2459344 <kmm2459344@stu.o-hara.ac.jp>
2024-07-19 14:06:49 +0900

    リバーシを作成する

---
+++ Reversi/Reversi/Mode.h
@@ -0,0 +1,16 @@
+//======================================
+//	リバーシ Mode
+//======================================
+// ★ここにインクルードガード(開始)を記入してください
+#ifndef MODE_H
+#define MODE_H
+
+typedef enum {
+	MODE_1P,
+	MODE_2P,
+	MODE_WATCH,
+	MODE_MAX,
+} Mode;
+
+// ★ここにインクルードガード(終了)を記入してください#pragma once
+#endif // !MODE_H
