d601492fa6ebe5e13ddefb91b6c34f9e39f4f366
kmm2459344 <kmm2459344@stu.o-hara.ac.jp>
2024-09-05 13:53:06 +0900

    //====================================== //      ユーティリティ //====================================== using System.Text; // StringBuilder
    namespace GP2
    {
        static class Utility
        {
            public const string EscBLACK   = "\x1b[30m";
            public const string EscRED     = "\x1b[31m";
            public const string EscGREEN   = "\x1b[32m";
            public const string EscYELLOW  = "\x1b[33m";
            public const string EscBLUE    = "\x1b[34m";
            public const string EscMAZENTA = "\x1b[35m";
            public const string EscCYAN    = "\x1b[36m";
            public const string EscWHITE   = "\x1b[37m";
            public const string EscDEFAULT = "\x1b[39m";
    
            public const string EscBgBLACK   = "\x1b[40m";
            public const string EscBgRED     = "\x1b[41m";
            public const string EscBgGREEN   = "\x1b[42m";
            public const string EscBgYELLOW  = "\x1b[43m";
            public const string EscBgBLUE    = "\x1b[44m";
            public const string EscBgMAZENTA = "\x1b[45m";
            public const string EscBgCYAN    = "\x1b[46m";
            public const string EscBgWHITE   = "\x1b[47m";
            public const string EscBgDEFAULT = "\x1b[49m";
    
            private static Random s_rand = new Random();
            // 乱数初期化
            public static void InitRand()
            {
                //s_rand = new Random();
            }
            // 乱数取得
            public static int GetRand(int max)
            {
                return s_rand.Next(max);
            }
            // キー入力待ち
            public static void WaitKey()
            {
                Console.ReadKey(true);
            }
            // キー取得
            //  ConsoleKey.UpArrow    カーソル上
            //  ConsoleKey.DownArrow  カーソル下
            //  ConsoleKey.LeftArrow  カーソル左
            //  ConsoleKey.RightArrow カーソル右
            //  ConsoleKey.Return     リターンキー
            //  ConsoleKey.Escape     ESCキー
            //  ConsoleKey.Spacebar   スペースキー
            public static ConsoleKey GetKey()
            {
                ConsoleKeyInfo info = Console.ReadKey(true);
                return info.Key;
            }
            // キーが押されたか?
            public static bool KeyAvailable()
            {
                return Console.KeyAvailable;
            }
    
            static StringBuilder s_console = new StringBuilder();
            //--------------------------------------
            //      コンソール出力
            //--------------------------------------
            // 使い方 : StringBuilderにため込んで PrintOut()で実際の描画を行う
            //    Utility.ClearScreen();
            //    Utilitu.Printf(....);
            //    Utility.Putchar(.);
            //    ....
            //    Utility.PrintOut();
            //--------------------------------------
            // 画面クリア
            public static void ClearScreen()
            {
                s_console.Clear();
            }
            // printf()関数
            public static void Printf(string fmt, params Object[] list)
            {
                s_console.AppendFormat(fmt, list);
            }
            // putchar()関数
            public static void Putchar(Char c)
            {
                s_console.Append(c);
            }
            // プリント出力
            public static void PrintOut()
            {
                Console.Clear();
                Console.Write(s_console.ToString());
            }
            // m秒スリープ
            public static void Sleep_mSec(int mSec)
            {
                System.Threading.Thread.Sleep(mSec);
            }
        } // class Utility
    } // namespace

---
+++ 3DDungeonGame_CS/UI.cs
@@ -0,0 +1,117 @@
+﻿//======================================
+//      疑似3Dダンジョン　UI
+//======================================
+using Utility = GP2.Utility;
+using Vector2 = GP2.Vector2;
+using System;  // ConsoleKey
+
+namespace _3DDungeonGame_CS
+{
+    internal static class UI
+    {
+        // プレーヤ移動( Exitフラグを返す)
+        public static bool MovePlayer(Stage stage)
+        {
+            ConsoleKey c = Utility.GetKey();
+            if (c == ConsoleKey.Spacebar)
+            {
+                stage.isForMap = !stage.isForMap;
+            }
+            else if (c == ConsoleKey.Escape)
+            {
+                return true;
+            }
+            else
+            {
+                if (stage.isForMap)
+                {
+                    MovePlayerForMap(stage, c);
+                }
+                else
+                {
+                    MovePlayerFor3D(stage, c);
+                }
+            }
+            return false;
+        }
+        public static void DrawOperation(Stage stage)
+        {
+            Utility.Puts("SPACE : change UI");
+            Utility.Puts("ESC : exit game");
+            if (stage.isForMap)
+            {
+                Utility.Puts("↑:北へ向く/北へ前進");
+                Utility.Puts("←:西へ向く/西へ前進");
+                Utility.Puts("→:東へ向く/東へ前進");
+                Utility.Puts("↓:南へ向く/南へ前進");
+            }
+            else
+            {
+                Utility.Puts("↑:前に進む");
+                Utility.Puts("←:左を向く");
+                Utility.Puts("→:右を向く");
+                Utility.Puts("↓:後ろを向く");
+            }
+        }
+        // プレーヤ移動(ForMap)
+        static void MovePlayerForMap(Stage stage, ConsoleKey c)
+        {
+            Character player = stage.player;
+            Direction d = Direction.Max;
+            switch (c)
+            {
+                case ConsoleKey.UpArrow: d = Direction.North; break;
+                case ConsoleKey.DownArrow: d = Direction.South; break;
+                case ConsoleKey.LeftArrow: d = Direction.West; break;
+                case ConsoleKey.RightArrow: d = Direction.East; break;
+            }
+            if (d != Direction.Max)
+            {
+                if (player.dir != d)
+                {
+                    player.dir = d;
+                }
+                else
+                {
+                    if (stage.GetMazeWall(player.pos, player.dir) == false)
+                    {
+                        Vector2 dir = Misc.GetDirVector2(d);
+                        Vector2 newPos = player.pos + dir;
+                        if (stage.IsInsideMaze(newPos))
+                        {
+                            player.pos = newPos;
+                        }
+                    }
+                }
+            }
+        }
+        // プレーヤ移動(For3D)
+        static void MovePlayerFor3D(Stage stage, ConsoleKey c)
+        {
+            Character player = stage.player;
+            switch (c)
+            {
+                case ConsoleKey.UpArrow:
+                    if (stage.GetMazeWall(player.pos, player.dir) == false)
+                    {
+                        Vector2 dir = Misc.GetDirVector2(player.dir);
+                        Vector2 newPos = player.pos + dir;
+                        if (stage.IsInsideMaze(newPos))
+                        {
+                            player.pos = newPos;
+                        }
+                    }
+                    break;
+                case ConsoleKey.DownArrow:
+                    player.TurnBack();
+                    break;
+                case ConsoleKey.LeftArrow:
+                    player.TurnLeft();
+                    break;
+                case ConsoleKey.RightArrow:
+                    player.TurnRight();
+                    break;
+            }
+        }
+    } // class
+} // namespace 
